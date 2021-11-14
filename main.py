65acdfcf69acfc38056e64940d17ac83ecf8dd17
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-11-14 17:43:20 +0200

    added the Esc button funtionality

--- main.py
+++ main.py
@@ -12,11 +12,28 @@ def start_screen(stdscr):
 	stdscr.getkey()
 
 
+def txt_test(stdscr):
+	target_text = 'JacksEpicYoutubeChannelFullofFunTimesAndfunHiRickX'
+	text = []
+	stdscr.clear()
+	stdscr.addstr(target_text)
+	stdscr.refresh()
+	stdscr.getkey()
+
+	while True:
+		key = stdscr.getkey()
+
+		if ord(key) == 27:
+			break
+		
+
+
 def main(stdscr):
 	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
 	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 	
 	start_screen(stdscr)
+	txt_test(stdscr)
 
 wrapper(main)
