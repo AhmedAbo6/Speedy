843575449d7a361e24f9a0f46d68fdd2be589fd2
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-11-14 18:28:54 +0200

    Fully funtioning app with static writing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import curses
 from curses import wrapper
 
@@ -15,10 +16,17 @@ def start_screen(stdscr):
 def txt_test(stdscr):
 	target_text = 'JacksEpicYoutubeChannelFullofFunTimesAndfunHiRickX'
 	text = []
+	wpm = 0
+	start_time = time.time()
+	stdscr.nodelay(True)
 
 	while True:
+		time_elapsed = max(time.time() - start_time, 1)
+		wpm = round((len(text) / (time_elapsed / 60)) / 5)
+
 		stdscr.clear()
 		stdscr.addstr(target_text)
+		stdscr.addstr(1, 0, f'WPM : {wpm}')
 
 		for i, char in enumerate(text):
 			correct_char = target_text[i]
@@ -29,25 +37,41 @@ def txt_test(stdscr):
 
 		stdscr.refresh()
 
-		key = stdscr.getkey()
+		if "".join(text) == target_text:
+			stdscr.nodelay(False)
+			break
+
+		try:
+			key = stdscr.getkey()
+		except:
+			continue
+		
 
 		if ord(key) == 27:
 			break
 		if key in ("KEY_BACKSPACE", '\b', "\x7f"):
 			if len(text) > 0:
 				text.pop()
-		else:
+		elif len(text) < len(target_text):
 			text.append(key)
 
 
 
 
 def main(stdscr):
-	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
 	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 	
 	start_screen(stdscr)
-	txt_test(stdscr)
+	while True:
+
+		txt_test(stdscr)
+
+		stdscr.addstr(2, 0, "Well Done! Press any key to continue....")
+		
+		key = stdscr.getkey()
+		if ord(key) == 27:
+			break
 
 wrapper(main)
