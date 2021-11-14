e426214ab3e18c6b817d02459315b12291830048
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-11-14 17:27:29 +0200

    added some colors

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from curses import wrapper
 
 
 def main(stdscr):
+	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 	stdscr.clear()
-	stdscr.addstr("Hi U!")
+	stdscr.addstr("Hi U!", curses.color_pair(1))
 	stdscr.refresh()
 	stdscr.getkey()
 
